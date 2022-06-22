8c9d3d52dc7aab03091b4d8d6c0c6765c1b359f5
ThinkerWel <92936147+ThinkerWel@users.noreply.github.com>
2022-06-23 00:29:26 +0300

    Add files via upload

---
+++ RandNumberGame/Learning.cpp.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable : 4996) .
+#include <iostream>
+#include  <string>
+#include "golf.h"
+#include <cctype>
+
+
+
+int main(void)
+{
+	Game one;
+	one= Game("Ruslan", "Anton");
+	int sum1, sum2;
+	std::cout << "Enter your deposit  #1 : ";
+	std::cin >> sum1;
+
+	std::cout << "Enter your deposit  #1 : ";
+	std::cin >> sum2;
+	one.GivePoint(sum1, sum2);
+
+	one.show();
+	one.GmA();
+	
+	
+
+	return 0;
+
+}
---
+++ RandNumberGame/golf.h
@@ -0,0 +1,23 @@
+#ifndef GOLF_H
+#define GOLF_H
+
+class Game {
+	std::string player_name1;
+	std::string player_name2;
+	int xd1, xd2;
+	int level1,level2;
+	int wallet, purse;
+
+public:
+	Game() {
+		player_name1 = "";
+		player_name2 = "";
+	}
+	void GmA();
+	Game(const std::string& name, const std::string name1);
+	void show()const;
+	void GivePoint(int wallet_t,int purse_t);
+};
+
+#endif
+    
