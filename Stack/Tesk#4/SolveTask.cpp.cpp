a5dfd15f858a41ef7ee950b7546f38457942aefc
ThinkerWel <92936147+ThinkerWel@users.noreply.github.com>
2022-07-14 00:50:28 +0300

    Add files via upload

---
+++ Stack/Tesk#4/SolveTask.cpp.cpp
@@ -0,0 +1,56 @@
+﻿#include "file1.h"
+#include <iostream>
+
+const int SIZE = 4;
+
+int main()
+{
+	using std::cin;
+	using std::cout;
+
+	Person* s[SIZE];
+
+	int i = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		char ch;
+		cout << "Enter you choice: ";
+		cin >> ch;
+		while (strchr("ertlq", ch) == NULL)
+		{
+			cout << "You need repear your choice: ";
+			cin >> ch;
+		}
+		cin.get();
+		if (ch == 'q')
+			break;
+
+		switch (ch)
+		{
+		case 'e':s[i] = new Person;
+			break;
+		case'r':s[i] = new Gunlinger;
+			break;
+		case't':PokerPlayer;
+			break;
+		case 'l':s[i] = new BadDude;
+			break;
+		}
+		s[i]->Set();
+		s[i]->Show();
+	
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		s[i]->Show();
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		delete s[i];
+	}
+
+	cout << "Done\n";
+	return 0;
+}
---
+++ Stack/Tesk#4/file1.h
@@ -0,0 +1,80 @@
+
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+class Person
+{
+private:
+	std::string fname;
+	std::string lname;
+public:
+	Person() :fname("Ruslan"), lname("Aslanov") {}
+	Person(const std::string& fn, const std::string& ln) :fname(fn), lname(ln) {}
+	virtual void Show()const {
+		std::cout << "Fname: "<< fname <<" "<< "Lname: " << lname << std::endl;
+		std::cout << std::endl;
+	}
+	
+	virtual void Set() {
+		cout << "Enter fname: ";
+		getline(cin, fname);
+		cout << "Enter lname";
+		getline(cin, lname);
+		
+	}
+};
+
+class Gunlinger :virtual public Person
+{
+private:
+	int num;
+	double time;
+public:
+	Gunlinger() :num(0), time(0.0) {}
+	Gunlinger(int num_t, double time_t) :num(num_t), time(time_t) {}
+	virtual void Show()const {
+		std::cout << "Number: " << num << "Time: " << time << std::endl;
+	}
+	virtual double Draw() { return time; }
+	virtual void Set() {
+		cout << "Enter number: ";
+		cin >> num;
+		cout << "Enter time: ";
+		cin >> time;
+	}
+};
+
+class PokerPlayer :virtual public Person
+{
+public:
+	int Draw() {
+
+		return rand()%52;
+	}
+	PokerPlayer(const std::string &s1,const std::string &s2) :Person(s1, s2) {}
+	virtual void Show()const { Person::Show(); }
+	virtual void Set() {
+		Person::Set();
+	}
+};
+
+class BadDude :public Gunlinger, public PokerPlayer
+{
+public:
+	BadDude(const std::string& fn=" ", const std::string& ln=" ", int n=0, double b=0.0) :Person(fn, ln), Gunlinger(n, b), PokerPlayer(fn, ln) {}
+	virtual void Show()const{
+		Person::Show();
+		Gunlinger::Show();
+		PokerPlayer::Show();
+	}
+
+	double  Gdraw() { return Gunlinger::Draw(); }
+	int Cdraw() { return PokerPlayer::Draw(); }
+	virtual void Set() {
+		Person::Set();
+		Gunlinger::Set();
+	}
+};
