ba7e11f03fd959b5ace4b8d9bbedd9e2deaf650b
ThinkerWel <92936147+ThinkerWel@users.noreply.github.com>
2022-07-20 20:14:16 +0300

    Update Job.cpp

--- Stack/Exception/Job.cpp
+++ Stack/Exception/Job.cpp
@@ -1,113 +1,8 @@
 #include "file1.h"
 #include <iostream>
 
-/*bool Tv::volup()
-{
-	if (volume < MaxVal)
-	{
-		volume++;
-		return true;
-	}
-	else
-		return false;
-}
-bool Tv::voldown()
-{
-	if (volume > MinVal) {
-		volume--;
-		return true;
-	}
-	else
-		return false;
-}
-void Tv::chanup()
-{
-	if (channel < maxchannel)
-		channel++;
-	else
-		channel = 1;
-}
-void Tv::chandown()
-{
-	if (channel > 1)
-		channel--;
-	else
-		channel = maxchannel;
-}
-void Tv::settings()const
-{
-	cout << "Tv is" << (state == Off ? "Off" : "On") << std::endl;
-	if (state ==On) {
-		cout << "Volume setting = " << volume << std::endl;
-		cout << "Channel setting = " << channel << std::endl;
-		cout << "Mode = "
-			<< (mode == Antenna ? "Antenna" : "Cable") << "\n";
-		cout<<"Input = "
-			<< (input == TV ? "TV":"DVD") << "\n";
-	}
-*/
 
-using std::string;
-
-/*Sales::bad_index::bad_index(int ix, const string& s)
-	:std::logic_error(s), bi(ix) {}
-
-Sales::Sales(int yy)
-{
-	year = yy;
-	for (int i = 0; i < MONTHS; i++)
-		gross[i] = 0;
-}
-Sales::Sales(int yy, const double* gr, int n)
-{
-	year = yy;
-	int lim = (n < MONTHS) ? n : MONTHS;
-	int i;
-	for (i = 0; i < lim; i++)
-		gross[i] = gr[i];
-	for (; i < MONTHS; i++)
-		gross[0] = 0;
-}
-double &  Sales::operator[](int i) {
-	if (i < 0 || i >= MONTHS)
-		throw bad_index(i);
-	return gross[i];
-}
-double Sales::operator[](int i)const {
-	if (i<0 || i>=MONTHS)
-		throw bad_index(i);
-	return gross[i];
-}
-LabeledSales::LabeledSales(const string& lb, int yy) :Sales(yy) {
-	label = lb;
-}
-LabeledSales::LabeledSales(const string& lb, int yy, const double* gr, int n) : Sales(yy, gr, n)
-{
-	label = lb;
-}
-LabeledSales::nbad_index::nbad_index(const string& lb, int ix, const string& s) : Sales::bad_index(ix, s)
-{
-	lbl = lb;
-}
-double LabeledSales::operator[](int i)const {
-	if (i < 0 || i >= MONTHS)
-		throw nbad_index(Label(), i);
-	return Sales::operator[](i);
-}
-double& LabeledSales::operator[](int i)
-{
-	if (i < 0 || i >= MONTHS)
-		throw nbad_index(Label(), i);
-	return Sales::operator[](i);
-}
-*/
-
-//int& RAM::operator[](int i)
-//{
-
-//}
-
-/*void RAM::Show()const {
+void RAM::Show()const {
 	LapTop::Data();
 	Data();
 }
@@ -179,5 +74,3 @@ void Windows::Set()
 	Get();
 }
 
-
-*/
